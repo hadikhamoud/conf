0cf1040da533deeb872fece6f06a5497815d8f83
Hadi Hamoud <hadikhamoud@gmail.com>
2025-12-27 11:43:59 +0300

    switched to kanagawa

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -25,13 +25,46 @@
 -- 	end,
 -- }
 
+-- return {
+--   {
+--     "0Risotto/rainbow12",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--       vim.cmd("colorscheme rainbow12")
+--     end,
+--   }
+-- }
+
 return {
-  {
-    "0Risotto/rainbow12",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd("colorscheme rainbow12")
-    end,
-  }
+	"rebelot/kanagawa.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("kanagawa").setup({
+			compile = false,
+			undercurl = true,
+			commentStyle = { italic = true },
+			functionStyle = {},
+			keywordStyle = { italic = true },
+			statementStyle = { bold = true },
+			typeStyle = {},
+			transparent = false,
+			dimInactive = false,
+			terminalColors = true,
+			colors = {
+				palette = {},
+				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+			},
+			overrides = function(colors)
+				return {}
+			end,
+			theme = "wave",
+			background = {
+				dark = "wave",
+				light = "lotus"
+			},
+		})
+		vim.cmd("colorscheme kanagawa")
+	end,
 }
