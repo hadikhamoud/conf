3b79811fe10cb405791718c7189627f699f28a41
Hadi Hamoud <hadikhamoud@gmail.com>
2026-03-13 13:24:05 +0300

    renamed and changed fish shell config

--- fish/config.fish
+++ fish/config.fish
@@ -12,5 +12,14 @@ if status is-interactive
 set -g PATH $PATH ~/google-cloud-sdk/bin
 set -x CLOUDSDK_PYTHON (which python3.11)
 
+set -gx PATH "/Users/hadihamoud/.local/state/fnm_multishells/77380_1772859005091/bin" $PATH;
+set -gx FNM_MULTISHELL_PATH "/Users/hadihamoud/.local/state/fnm_multishells/77380_1772859005091";
+set -gx FNM_VERSION_FILE_STRATEGY "local";
+set -gx FNM_DIR "/Users/hadihamoud/.local/share/fnm";
+set -gx FNM_LOGLEVEL "info";
+set -gx FNM_NODE_DIST_MIRROR "https://nodejs.org/dist";
+set -gx FNM_COREPACK_ENABLED "false";
+set -gx FNM_RESOLVE_ENGINES "true";
+set -gx FNM_ARCH "arm64";
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/hadihamoud/google-cloud-sdk/path.fish.inc' ]; . '/Users/hadihamoud/google-cloud-sdk/path.fish.inc'; end
